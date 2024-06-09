55a5fafef14a140f98257a78c98cf7254297775e
57Ajay <57ajay.u@gmail.com>
2024-06-09 16:27:38 +0530

    adding Types into Objects and other methods

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,53 @@
 "use strict";
-const Name = "Ajay";
-console.log(Name);
+const sum = (a, b) => a + b;
+console.log(sum(1, 2));
+const arr = ["Ajay", "Aman", "vijay"];
+const numArray = [1, 5, 6, 7];
+const aTuple = ["Ajay", false, 21];
+console.log(`${arr},\n ${numArray}, \n ${aTuple}`);
+//Objects
+let myObj = {
+    Name: "Ajay",
+    Age: 21,
+    hasJob: false,
+    add: (a, b) => a + b,
+    anArray: [1, 2, 3, 4, 5],
+};
+console.log(typeof myObj);
+let evh = {
+    Name: "Eddie",
+    Age: 21,
+    active: true,
+    albums: [1984, "interviews", 5150, "The Killers"],
+};
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.Name}`;
+};
+console.log(greetGuitarist(evh));
+;
+const Swift = {
+    Name: "Taylor Swift",
+    active: true,
+    sing: () => console.log("Lavender Haze"),
+    albums: [1984, "interviews", 5150, "The Killers"],
+};
+const greetSinger = (singer) => {
+    singer.sing();
+    return `Hello ${singer.Name}`;
+};
+console.log(greetSinger(Swift));
+//Enums
+var grade;
+(function (grade) {
+    grade[grade["U"] = 0] = "U";
+    grade[grade["D"] = 2] = "D";
+    grade[grade["C"] = 3] = "C";
+    grade[grade["B"] = 4] = "B";
+    grade[grade["A"] = 5] = "A";
+})(grade || (grade = {}));
+;
+console.log(grade.A);
+const arr1 = ["Ajay", 21, "vijay"];
+//literal Types
+let userNames;
+userNames = "Ajay";
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,84 @@
-const Name: string = "Ajay";
 
-console.log(Name);
+const sum = (a: number, b: number) => a + b;
+console.log(sum(1, 2));
+
+const arr: string[] = ["Ajay", "Aman", "vijay"];
+
+const numArray: number[] = [1, 5, 6, 7];
+
+const aTuple: [string, boolean, number] = ["Ajay", false, 21];
+console.log(`${arr},\n ${numArray}, \n ${aTuple}`);
+
+//Objects
+
+let myObj: object = {
+    Name: "Ajay",
+    Age: 21,
+    hasJob: false,
+    add: (a: number, b: number) => a + b,
+    anArray: [1, 2, 3, 4, 5],
+};
+
+console.log(typeof myObj);
+type Guitarist = {
+    Name: string,
+    Age?: number,
+    active: boolean,
+    albums: (string | number)[]
+};
+
+let evh: Guitarist = {
+    Name: "Eddie",
+    Age: 21,
+    active: true,
+    albums: [1984, "interviews", 5150, "The Killers"],
+};
+
+const greetGuitarist = (guitarist: Guitarist) =>{
+    return `Hello ${guitarist.Name}`
+};
+console.log(greetGuitarist(evh));
+
+interface Singer{
+    Name: string,
+    active: boolean,
+    sing: () => void,
+    albums: (string | number)[]
+};
+
+const Swift: Singer = {
+    Name: "Taylor Swift",
+    active: true,
+    sing: () => console.log("Lavender Haze"),
+    albums: [1984, "interviews", 5150, "The Killers"],
+}
+const greetSinger = (singer: Singer) =>{
+    singer.sing()
+    return `Hello ${singer.Name}`
+};
+console.log(greetSinger(Swift));
+
+//Enums
+
+enum grade {
+    U,
+    D = 2,//others will adapt from 3
+    C,
+    B,
+    A
+};
+console.log(grade.A)
+
+//Type Aliases
+
+type stringOrNUmber = string | number;
+type stringOrNUmberArray = stringOrNUmber[];
+
+const arr1: stringOrNUmberArray = ["Ajay", 21, "vijay"];
+
+//literal Types
+
+let userNames : "Ajay" | "Aman" | "Sekhar";
+userNames = "Ajay";
+
+
