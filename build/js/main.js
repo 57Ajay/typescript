91e170eae2124a578c2b884266878b7427508a71
57Ajay <57ajay.u@gmail.com>
2024-06-09 18:04:59 +0530

    adding type Assertions

--- build/js/main.js
+++ build/js/main.js
@@ -51,3 +51,63 @@ const arr1 = ["Ajay", 21, "vijay"];
 //literal Types
 let userNames;
 userNames = "Ajay";
+/**
+ * interface mathFunctions{
+ * (a: number,
+ * b: number): number
+ * } //Similar to above
+ *
+ * @param a
+ * @param b
+ * @returns
+ */
+const multiply = (a, b) => a * b;
+const subtract = (x, y) => x - y;
+console.log(multiply(2, 3));
+console.log(subtract(21, 3));
+//Optional Parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== "undefined") {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+console.log(`${addAll(1, 2)}, ${sumAll(1, 2, 3)}, ${sumAll(1, 2)}, ${addAll(11, 25, 233)}`);
+//Rest Parameters
+const total = (...numbers) => {
+    return numbers.reduce((a, b) => a + b);
+};
+console.log(total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30));
+//never types
+// const createError = (message: string): never =>{
+//     throw new Error(message);
+// };
+// console.log(`${createError("An Error Occured created by You")}`);
+const numOrStr = (a) => {
+    if (typeof a === "string") {
+        return "string";
+    }
+    if (typeof a === "number") {
+        return "number";
+    }
+    return createError("This Should Never be happening");
+};
+console.log(`${numOrStr(1)}, ${numOrStr("1")}, ${numOrStr("")}`);
+function createError(arg0) {
+    throw new Error("Function not implemented.");
+}
+//converts to more or less Specific;
+let a = "hello";
+let b = a; // less Specific
+let c = a; // More Specific
+let d = "world";
+let e = "world";
+const addOrConcat = (a, b, c) => {
+    return c === 'add' ? a + b : `${a} ${b}`;
+};
+console.log(`${addOrConcat(1, 2, 'add')}, ${addOrConcat(1, 2, 'concat')}`);
+let result = addOrConcat(2, 2, 'concat');
+console.log(result);
--- src/main.ts
+++ src/main.ts
@@ -81,4 +81,89 @@ const arr1: stringOrNUmberArray = ["Ajay", 21, "vijay"];
 let userNames : "Ajay" | "Aman" | "Sekhar";
 userNames = "Ajay";
 
+//functions
 
+type mathFunctions = (a: number, b: number)=> number;
+/**
+ * interface mathFunctions{
+ * (a: number,
+ * b: number): number
+ * } //Similar to above
+ * 
+ * @param a
+ * @param b 
+ * @returns 
+ */
+const multiply: mathFunctions = (a, b) => a * b;
+const subtract: mathFunctions = (x, y)=> x - y;
+
+console.log(multiply(2, 3));
+console.log(subtract(21, 3));
+
+//Optional Parameters
+
+const addAll = (a: number, b: number, c?: number): number=>{
+    if (typeof c !== "undefined"){
+        return a + b + c;
+    }return a+b
+}
+const sumAll = (a: number, b: number, c: number=2): number=>{
+    return a+b+c
+}
+
+console.log(`${addAll(1, 2)}, ${sumAll(1, 2, 3)}, ${sumAll(1, 2)}, ${addAll(11, 25, 233)}`);
+
+//Rest Parameters
+
+const total = (...numbers: number[]): number =>{
+    return numbers.reduce((a, b) => a + b)
+}
+console.log(total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30));
+
+//never types
+
+// const createError = (message: string): never =>{
+//     throw new Error(message);
+// };
+
+// console.log(`${createError("An Error Occured created by You")}`);
+
+const numOrStr = (a: number | string): string =>{
+    if(typeof a === "string"){
+        return "string"
+    }if(typeof a === "number"){
+        return "number"
+    }return createError("This Should Never be happening");
+};
+
+console.log(`${numOrStr(1)}, ${numOrStr("1")}, ${numOrStr("")}`);
+
+function createError(arg0: string): string {
+    throw new Error("Function not implemented.");
+}
+
+// Type Assertions
+
+type one = string;
+type two = string | number;
+type three = 'hello';
+
+//converts to more or less Specific;
+
+let a: one = "hello";
+let b = a as two;// less Specific
+let c = a as three;// More Specific
+
+let d = <one> "world";
+let e = <string> "world";
+
+
+const addOrConcat = (a: number, b: number, c: 'add'| 'concat'): number | string =>{
+    return c === 'add' ? a + b : `${a} ${b}`;
+};
+
+console.log(`${addOrConcat(1, 2, 'add')}, ${addOrConcat(1, 2, 'concat')}`);
+
+let result : string = addOrConcat(2, 2, 'concat') as string;
+
+console.log(result);
